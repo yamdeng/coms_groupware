6f20bd0249ae24189fe250ab7a12208407cd6c03
yamdeng <yamdeng@gmail.com>
2022-12-17 12:23:30 +0900

    정리

--- 실제사용_DB작업.sql
+++ 실제사용_DB작업.sql
@@ -48,6 +48,37 @@ FROM information_schema.columns a
 WHERE a.table_name = lower('OFFICE_COMMUTE_DAY')
 order by a.ordinal_position) As data_type_comment;
 
+-- resultMap 추출
+SELECT concat(
+	'<result property=@', (lower(substring(pascal_case,1,1)) || substring(pascal_case,2)), '@ column=@', column_name, '@/>'
+	) as java_vo_string
+FROM (
+SELECT a.column_name
+	,replace(initcap(replace(a.column_name, '_', ' ')), ' ', '') As pascal_case
+	,CASE WHEN a.data_type in('character', 'character varying', 'text') THEN 'String'
+			WHEN a.data_type in('timestamp without time zone', 'timestamp') THEN 'LocalDateTime'
+			WHEN a.data_type in('numeric') THEN 'Double'
+			WHEN a.data_type in('integer') THEN 'Integer'
+            ELSE
+             	''
+            END AS java_type
+	,b.COLUMN_COMMENT
+FROM information_schema.columns a
+	inner join (
+		SELECT
+			PS.RELNAME AS TABLE_NAME,
+			PA.ATTNAME AS COLUMN_NAME,
+			PD.DESCRIPTION AS COLUMN_COMMENT
+		FROM PG_STAT_ALL_TABLES PS, PG_DESCRIPTION PD, PG_ATTRIBUTE PA
+		WHERE PD.OBJSUBID<>0
+			AND PS.RELID=PD.OBJOID
+			AND PD.OBJOID=PA.ATTRELID
+			AND PD.OBJSUBID=PA.ATTNUM
+			AND PS.RELNAME= lower('OFFICE_COMMUTE_DAY') ) b
+			on a.column_name = b.column_name
+WHERE a.table_name = lower('OFFICE_COMMUTE_DAY')
+order by a.ordinal_position) As data_type_comment;
+
 -- insert / update column 추출 : ,BASE_DATE_STR
 select concat(',', upper(column_name))
 from information_schema.columns
